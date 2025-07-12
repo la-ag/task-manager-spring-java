9e270efd59ce2b6e458925e0cdac5c981dda72a9
Lakshya Agarwal <lakshya.agarwal5@gmail.com>
2025-07-12 19:24:23 +0530

    basic project files

--- src/main/java/com/scaler/taskmanager/TaskManagerSpringJavaApplication.java
+++ src/main/java/com/scaler/taskmanager/TaskManagerSpringJavaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TaskManagerSpringJavaApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TaskManagerSpringJavaApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerSpringJavaApplication.class, args);
+	}
 
 }
--- src/test/java/com/scaler/taskmanager/TaskManagerSpringJavaApplicationTests.java
+++ src/test/java/com/scaler/taskmanager/TaskManagerSpringJavaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TaskManagerSpringJavaApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
